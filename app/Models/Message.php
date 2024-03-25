302242202765fff8ea4d6e0d1320edd9c081e3f4
boolfalse <extravaganza1024@gmail.com>
2024-03-24 17:59:28 -0700

    Message - model & migration

---
+++ app/Models/Message.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Message extends Model
+{
+    use HasFactory;
+
+    public $table = 'messages';
+
+    protected $fillable = [
+        'id',
+        'user_id',
+        'text',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function getTimeAttribute(): string
+    {
+        return date("d M Y | H:i:s", strtotime($this->attributes['created_at']));
+    }
+}
