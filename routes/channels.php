4af4a5befd1a1501654aa12741240bcf673d8edd
boolfalse <extravaganza1024@gmail.com>
2024-03-26 03:25:34 -0700

    channels setup

--- routes/channels.php
+++ routes/channels.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Broadcast;
 
-Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+//Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
+//    return (int) $user->id === (int) $id;
+//});
+
+Broadcast::channel('channel_for_everyone', function ($user) {
+    return true;
 });
